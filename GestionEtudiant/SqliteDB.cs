5796bf0325ee7e69f8335f73dc5392c1d5c23f29
aimanmk <malkouniman@gmail.com>
2021-02-15 12:46:37 +0000

    addded a simple sqliteDb

---
+++ GestionEtudiant/SqliteDB.cs
@@ -0,0 +1,75 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SQLite;
+using System.IO;
+
+namespace GestionEtudiant
+{
+    class SqliteDB
+    {
+        string dbPath = Path.Combine( System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "Gestion.db3");
+
+
+        [Table("Etudiant")]
+        public class Etudiant
+        {
+            [PrimaryKey, AutoIncrement, Column("_id")]
+            public int Cin { get; set; }
+            [MaxLength(10)]
+            public string Nom { get; set; }
+            [MaxLength(10)]
+            public string Prenom { get; set; }
+
+            [MaxLength(28)]
+            public string Email { get; set; }
+            [MaxLength(28)]
+            public string Tel { get; set; }
+            public int IdFiliere { get; set; }
+            public int IdMatiere { get; set; }
+
+            public int NbAbsence { get; set; }
+            public int NbPresence { get; set; }
+            [MaxLength(28)]
+            public string Absent { get; set; }
+
+        }
+
+
+        [Table("Filiere")]
+        public class Filiere
+        {
+            [PrimaryKey, AutoIncrement, Column("_id")]
+            public int Id { get; set; }
+            [MaxLength(8)]
+            public string NomFiliere { get; set; }
+        }
+
+        [Table("Matiere")]
+        public class Matiere
+        {
+            [PrimaryKey, AutoIncrement, Column("_id")]
+            public int Id { get; set; }
+            [MaxLength(8)]
+            public string NomMatiere { get; set; }
+        }
+
+        [Table("Professeur")]
+        public class Professeur
+        {
+            [PrimaryKey, AutoIncrement, Column("_id")]
+            public int Id { get; set; }
+            [MaxLength(8)]
+            public string Login { get; set; }
+            [MaxLength(8)]
+            public string Password { get; set; }
+        }
+    }
+}
